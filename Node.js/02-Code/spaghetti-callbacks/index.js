784e3e0ee2501170a8c4dbce9a2eeefbe5f9564a
Oscar Renalias <oscar.renalias@accenture.com>
2013-09-09 08:34:01 +0300

    Added some more code

---
+++ Node.js/02-Code/spaghetti-callbacks/index.js
@@ -0,0 +1,10 @@
+var db=require("somedatabase");
+
+app.get("/price", function(req, res) {
+	db.openConnection("host", port, function(error, conn) {
+		conn.query("SELECT * FROM products WHERE id=?", [ req.param("product")], function(err, results) {
+			conn.close();
+			res.send(results[0]);
+		})
+	})
+});
